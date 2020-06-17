943c7d37ee51b433ed7e395bc10103d770c4f0d4
Rauan <rkoshkinbayev@gmail.com>
2020-06-17 23:34:39 +0600

    add realm service

--- MyNotes/Modules/View/MainTabbarController.swift
+++ MyNotes/Modules/View/MainTabbarController.swift
@@ -22,16 +22,4 @@ class MainTabbarController: CircleBarController {
         collectionController.tabBarItem = UITabBarItem(title: "", image: UIImage(named: "collectionMenu"), tag: 1)
         viewControllers = [tableController, collectionController]
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ MyNotes/Realm Service/RealmService.swift
@@ -0,0 +1,38 @@
+//
+//  RealmService.swift
+//  MyNotes
+//
+//  Created by rau4o on 6/17/20.
+//  Copyright © 2020 rau4o. All rights reserved.
+//
+
+import RealmSwift
+
+class RealmService {
+    
+    private init() {}
+    
+    static let shared = RealmService()
+    
+    var realm = try! Realm()
+    
+    func create<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.add(object)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func delete<T: Object>(_ object: T) {
+        do {
+            try realm.write {
+                realm.delete(object)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+}
